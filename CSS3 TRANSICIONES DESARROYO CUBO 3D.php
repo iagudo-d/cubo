11cade7e81032f3fb82428518b3293f2f976881d
Ivan Agudo Diaz <deeperblue6@gmail.com>
2022-11-08 03:13:19 +0100

    prieba

---
+++ CSS3 TRANSICIONES DESARROYO CUBO 3D.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>JAVASCRIPT RESOLUCION NAVEGADOR</title>
+		<!--<meta content="4" http-equiv="REFRESH"> </meta>-->
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8"> 
+		<link rel="stylesheet" type="text/css" href="CSS3 TRANSICIONES DESARROYO CUBO 3D.css"/>
+	</head>
+	<body>
+	<div class="container">
+		<div class="tras">
+			<div class="dere">
+				<div class="infe">
+					<div class="izqu">
+						<div class="supe">
+							<div class="tapa"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	</body>
+</html>
